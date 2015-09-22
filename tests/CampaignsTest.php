2f9ec3e74e1b2b496f5bf941755038e4e0bd6a3c
raka aditya <adm@rakaaditya.com>
2015-09-22 11:58:49 +0700

    Update campaign testing

--- tests/CampaignsTest.php
+++ tests/CampaignsTest.php
@@ -7,13 +7,15 @@ class CampaignsTest extends TestCase
     {
       $response = $this->call('GET', '/api/v1/campaigns');
       $this->assertEquals(200, $response->getStatusCode());
-      // $data = json_decode($response->getContent(), true);
-      //
-      // $this->assertNotEmpty($data['campaigns']);
-      // $this->assertNotEmpty($data['meta']);
-      // $this->assertNotEmpty($data['meta']['total']);
-      // $this->assertNotEmpty($data['meta']['page']);
-      // $this->assertNotEmpty($data['meta']['current_total']);
+      $data = json_decode($response->getContent(), true);
+
+      $this->assertNotEmpty($data['data']);
+      $this->assertNotEmpty($data['total']);
+      $this->assertNotEmpty($data['per_page']);
+      $this->assertNotEmpty($data['current_page']);
+      $this->assertNotEmpty($data['last_page']);
+      $this->assertNotEmpty($data['from']);
+      $this->assertNotEmpty($data['to']);
     }
 
 }
